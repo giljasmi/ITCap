7a76376124aae0383e4b61ec2caf357bea271772
bedlamiteartist <karan99130@gmail.com>
2015-09-24 15:43:35 -0400

    Empty Template

---
+++ InterviewTube/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterviewTube.Startup))]
+namespace InterviewTube
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

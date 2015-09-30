d2893164fe6afb3b2f3f7f15cd775de9c86cf95e
bedlamiteartist <karan99130@gmail.com>
2015-09-30 16:33:57 -0400

    Web cam accessed

---
+++ InterviewTube/Controllers/CamRecordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewTube.Controllers
+{
+    public class CamRecordController : Controller
+    {
+        //
+        // GET: /CamRecord/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
